7544d8dd28e922717bd1fe5f336162a12f1988f6
Elliot Cross <elliotcross27@gmail.com>
2020-06-11 16:26:16 +0100

    added basic ftps server deployment and started on phpmyadmin

---
+++ containers/ftps/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#generate private and public key for ftps server, both in pure-ftpd.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem \
+-subj "/C=EN/ST=./L=./O=42/CN=services_ftps"
+
+#make it private
+chmod 600 /etc/ssl/private/*.pem
+
+#add new user using environment variables from ftps.yaml
+adduser -D "$FTPS_USER"
+echo "$FTPS_USER:$FTPS_PASSWORD" | chpasswd
