1f7020a1f000c8feef8c7ec5715024f05e1d24d2
Elliot Cross <elliotcross27@gmail.com>
2020-06-24 17:40:34 +0100

    adding some automation to metallb setup

--- get_ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-minikube_ip=$(minikube ip)
-nums=$(echo $minikube_ip | tr "." "\n")
-arr=($nums)
-min="${arr[0]}.${arr[1]}.${arr[2]}.$((${arr[3]} + 5))"
-max="${arr[0]}.${arr[1]}.${arr[2]}.$((${arr[3]} + 20))"
-sed -i '' "s/MIN_IP/${min}/g" yaml_files/metallb.yaml
-sed -i '' "s/MAX_IP/${max}/g" yaml_files/metallb.yaml
--- setup.sh
+++ setup.sh
@@ -22,9 +22,22 @@ metallb_max_ip="${arr[0]}.${arr[1]}.${arr[2]}.$((${arr[3]} + 20))"
 sed -i '' "s/MIN_IP/${metallb_min}/g" yaml_files/metallb.yaml
 sed -i '' "s/MAX_IP/${metallb_max}/g" yaml_files/metallb.yaml
 
-sed -i '' "s/MIN_IP/${metallb_min}/g" yaml_files/metallb.yaml
-sed -i '' "s/MAX_IP/${metallb_max}/g" yaml_files/metallb.yaml
+# put lowest ip in all yaml files so all services share same ip
+for f in yaml_files/*
+do
+	if [ -d "$f" ]; then
+		continue
+	fi
+	sed -i '' "s/MIN_IP/${metallb_min_ip}/g" $f
+done
 
-# put back placeholders in metallb.yaml
+# if minikube needs to be restarted put back placeholders in yaml files
 #sed -i '' "s/${metallb_min}/MIN_IP/g" yaml_files/metallb.yaml
 #sed -i '' "s/${metallb_max}/MAX_IP/g" yaml_files/metallb.yaml
+#for f in yaml_files/*
+#do
+#	if [ -d "$f" ]; then
+#		continue
+#	fi
+#	sed -i '' "s/MIN_IP/${metallb_min_ip}/g" $f
+#done
