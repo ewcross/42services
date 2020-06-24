700281ccfdb4d4e749c92264713abe1ac2f1e927
Elliot Cross <elliotcross27@gmail.com>
2020-06-24 12:50:46 +0100

    finishing metallb config with bash script

--- metallb_setup.sh
+++ metallb_setup.sh
@@ -1,4 +1,8 @@
-#!bin/sh
+#!bin/bash
+
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
