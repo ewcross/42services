01d83d7600c36de34266edce7fc7d62f8787dbd7
Elliot Cross <elliotcross27@gmail.com>
2020-11-16 10:10:43 +0000

    working on nginx for wordpress instance

---
+++ srcs/containers/wordpress_nginx/add_functions.php
@@ -0,0 +1,25 @@
+
+function add_admin_acct(){
+
+	$login = 'admin';
+	$passw = 'admin';
+	$email = '';
+	if ( !username_exists( $login ) && !email_exists( $email ) ) {
+		$user_id = wp_create_user( $login, $passw, $email );
+		$user = new WP_User( $user_id );
+		$user->set_role( 'administrator' );
+	}
+}
+function add_editor_acct($login, $passw){
+
+	$email = '';
+	if ( !username_exists( $login ) && !email_exists( $email ) ) {
+		$user_id = wp_create_user( $login, $passw, $email );
+		$user = new WP_User( $user_id );
+		$user->set_role( 'editor' );
+	}
+}
+add_action('init','add_admin_acct');
+add_action('add_editor','add_editor_acct', 10, 2);
+do_action('add_editor', 'user1', 'user1');
+do_action('add_editor', 'user2', 'user2');
---
+++ srcs/containers/wordpress_nginx/init.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+sed -i 's/#   include "mod_fastcgi.conf"/include "mod_fastcgi.conf"/g' /etc/lighttpd/lighttpd.conf
+sed -i 's/php-cgi/php-cgi7/g' /etc/lighttpd/mod_fastcgi.conf
+
+# put environment variables (defined in wp.yaml) in wp-config file
+sed -i 's/WP_DB_NAME/'$WP_DB_NAME'/g' /var/www/localhost/htdocs/wordpress/wp-config.php
+sed -i 's/WP_DB_USER/'$WP_DB_USER'/g' /var/www/localhost/htdocs/wordpress/wp-config.php
+sed -i 's/WP_DB_PASSWORD/'$WP_DB_PASSWORD'/g' /var/www/localhost/htdocs/wordpress/wp-config.php
+sed -i 's/WP_DB_HOST/'$WP_DB_HOST'/g' /var/www/localhost/htdocs/wordpress/wp-config.php
+
+# add code to end of functions.php to create admin user and multiple editors
+cat add_functions.php >> /var/www/localhost/htdocs/wordpress/wp-content/themes/twentytwenty/functions.php
+
+openrc
+touch /run/openrc/softlevel
+rc-update add lighttpd default
+rc-service lighttpd start
+
+# without telegraf need this
+while : ;
+do sleep 1;
+done
