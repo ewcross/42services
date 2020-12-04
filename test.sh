8a3b38d262ed313c37f6334f08a0f1f434a10bff
Elliot Cross <elliotcross27@gmail.com>
2020-12-04 15:23:36 +0000

    fixing display issues with wp and php

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-kubectl delete svc phpmyadmin
-kubectl delete deployment phpmyadmin
+# kubectl delete svc phpmyadmin
+# kubectl delete deployment phpmyadmin
 
 # kubectl delete svc nginx
 # kubectl delete deployment nginx
 
-# kubectl delete svc wordpress
-# kubectl delete deployment wordpress
+kubectl delete svc wordpress
+kubectl delete deployment wordpress
 # kubectl delete pv wordpress-pv
 # kubectl delete pvc wordpress-pvc
 
@@ -17,13 +17,13 @@ kubectl delete deployment phpmyadmin
 # kubectl delete pvc mysql-pvc
 
 # docker build -t nginx:v1 srcs/containers/nginx/
-# docker build -t wordpress:v1 srcs/containers/wordpress/
+docker build -t wordpress:v1 srcs/containers/wordpress/
 # docker build -t mysql:v1 srcs/containers/mysql/
-docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
+# docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
 
 # kubectl apply -f srcs/yaml_files/nginx.yaml
 
-# kubectl apply -f srcs/yaml_files/wordpress.yaml
+kubectl apply -f srcs/yaml_files/wordpress.yaml
 # kubectl apply -f srcs/yaml_files/volumes/wordpress-pvc.yaml
 # kubectl apply -f srcs/yaml_files/volumes/wordpress-pv.yaml
 
@@ -31,6 +31,6 @@ docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
 # kubectl apply -f srcs/yaml_files/volumes/mysql-pvc.yaml
 # kubectl apply -f srcs/yaml_files/volumes/mysql-pv.yaml
 
-kubectl apply -f srcs/yaml_files/phpmyadmin.yaml
+# kubectl apply -f srcs/yaml_files/phpmyadmin.yaml
 
 # kubectl apply -f srcs/yaml_files/metallb.yaml
