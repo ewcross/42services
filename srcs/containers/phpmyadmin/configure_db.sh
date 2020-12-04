85460c51a119a068f1e139a22d404063e81b6a66
Elliot Cross <elliotcross27@gmail.com>
2020-12-04 12:15:26 +0000

    adding supervisord to php container

--- srcs/containers/phpmyadmin/configure_db.sh
+++ srcs/containers/phpmyadmin/configure_db.sh
@@ -6,6 +6,6 @@ sed -i 's/DB_PORT/'$DB_PORT'/g' /www/localhost/phpmyadmin/config.inc.php
 sed -i 's/DB_USER/'$DB_USER'/g' /www/localhost/phpmyadmin/config.inc.php
 sed -i 's/DB_PASSWORD/'$DB_PASSWORD'/g' /www/localhost/phpmyadmin/config.inc.php
 
-openrc
-touch /run/openrc/softlevel
-rc-service php-fpm7 start
+# openrc
+# touch /run/openrc/softlevel
+# rc-service php-fpm7 start
--- test.sh
+++ test.sh
@@ -3,10 +3,14 @@
 kubectl delete svc phpmyadmin
 kubectl delete deployment phpmyadmin
 
+# kubectl delete svc nginx
+# kubectl delete deployment nginx
+# kubectl delete svc wordpress
+# kubectl delete deployment wordpress
+
 # docker build -t nginx:v1 srcs/containers/nginx/
 # docker build -t wordpress:v1 srcs/containers/wordpress/
 # docker build -t mysql:v1 srcs/containers/mysql/
-
 docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
 
 # kubectl apply -f srcs/yaml_files/nginx.yaml
@@ -21,4 +25,4 @@ docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
 
 kubectl apply -f srcs/yaml_files/phpmyadmin.yaml
 
-kubectl apply -f srcs/yaml_files/metallb.yaml
+# kubectl apply -f srcs/yaml_files/metallb.yaml
