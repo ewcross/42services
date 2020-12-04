01a61ad34f70fd38362b78a9cb54761585d6daf2
Elliot Cross <elliotcross27@gmail.com>
2020-12-04 15:57:51 +0000

    sorted out redirect and proxy_pass

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 # kubectl delete svc phpmyadmin
 # kubectl delete deployment phpmyadmin
 
-# kubectl delete svc nginx
-# kubectl delete deployment nginx
+kubectl delete svc nginx
+kubectl delete deployment nginx
 
-kubectl delete svc wordpress
-kubectl delete deployment wordpress
+# kubectl delete svc wordpress
+# kubectl delete deployment wordpress
 # kubectl delete pv wordpress-pv
 # kubectl delete pvc wordpress-pvc
 
@@ -16,14 +16,14 @@ kubectl delete deployment wordpress
 # kubectl delete pv mysql-pv
 # kubectl delete pvc mysql-pvc
 
-# docker build -t nginx:v1 srcs/containers/nginx/
-docker build -t wordpress:v1 srcs/containers/wordpress/
+docker build -t nginx:v1 srcs/containers/nginx/
+# docker build -t wordpress:v1 srcs/containers/wordpress/
 # docker build -t mysql:v1 srcs/containers/mysql/
 # docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
 
-# kubectl apply -f srcs/yaml_files/nginx.yaml
+kubectl apply -f srcs/yaml_files/nginx.yaml
 
-kubectl apply -f srcs/yaml_files/wordpress.yaml
+# kubectl apply -f srcs/yaml_files/wordpress.yaml
 # kubectl apply -f srcs/yaml_files/volumes/wordpress-pvc.yaml
 # kubectl apply -f srcs/yaml_files/volumes/wordpress-pv.yaml
 
